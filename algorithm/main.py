5db44e37d9d36e7f6307b63a31fe74badea53345
Sandro Lutz <code@temparus.ch>
2022-09-18 04:05:51 +0200

    Cleanup of Python code

--- algorithm/main.py
+++ algorithm/main.py
@@ -6,179 +6,80 @@ from scipy.special import comb
 # args setting
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('-i', "--input", help="input file video")
-parser.add_argument('--leftPoint', type=int, help="Left rail offset", default=450)
-parser.add_argument('--rightPoint', type=int, help="Right rail offset", default=840)
-parser.add_argument('--topPoint', type=int, help="Top rail offset", default=330)
 args = parser.parse_args()
 
 
 def main():
     # load video class
     cap = VideoCapture(args.input)
+    out = cv2.VideoWriter('output/output.mp4', cv2.VideoWriter_fourcc(*'MP4V'), 20.0, (1280,720))
 
-    # initialization for line detection
-    expt_startLeft = args.leftPoint
-    expt_startRight = args.rightPoint
-    expt_startTop = args.topPoint
+    while True:
+        frame = cap.read()
 
-    # value initialize
-    left_maxpoint = [0] * 50
-    right_maxpoint = [195] * 50
-
-    # convolution filter
-    kernel = np.array([
-        [-1, 1, 0, 1, -1],
-        [-1, 1, 0, 1, -1],
-        [-1, 1, 0, 1, -1],
-        [-1, 1, 0, 1, -1],
-        [-1, 1, 0, 1, -1]
-    ])
-
-    # Next frame availability
-    r = True
-    first = True
-
-    while r is True:
-        r, frame = cap.read()
+        # Algorithm is stable when reading in the same image consequently.
+        # With the produced video file, there are still some hickups.
+        # frame = cv2.imread('video/track_test_image_resized.jpg')
         if frame is None:
             break
 
-        # cut away invalid frame area
-        # valid_frame = frame[expt_startTop:, expt_startLeft:expt_startRight]
         valid_frame = frame.copy()
-        # original_frame = valid_frame.copy()
 
         # gray scale transform
         gray_frame = cv2.cvtColor(valid_frame, cv2.COLOR_BGR2GRAY)
+        hsv_frame = cv2.cvtColor(valid_frame,cv2.COLOR_BGR2HSV)
 
         # histogram equalization image
         histeqaul_frame = cv2.equalizeHist(gray_frame)
 
-        # apply gaussian blur
-        blur_frame = cv2.GaussianBlur(histeqaul_frame, (5, 5), 5)
+        # We subtract the s channel of the HSV colored image to remove
+        # shadows from the image.
+        s = hsv_frame[:, :, 1]
+        histeqaul_frame_s = cv2.subtract(histeqaul_frame, s)
 
-        # merge current frame and last frame
-        merge_frame = blur_frame
-        # if first is True:
-        #     merge_frame = blur_frame
-        #     first = False
-        #     old_valid_frame = merge_frame.copy()
-        # else:
-        #     merge_frame = cv2.addWeighted(blur_frame, 0.2, old_valid_frame, 0.8, 0)
-        #     old_valid_frame = merge_frame.copy()
+        # Perform edge detection with the Canny Algorithm
+        height, width = histeqaul_frame_s.shape
+        edges = cv2.Canny(histeqaul_frame_s, 59, 3*59, 3)
 
-        edges = cv2.Canny(merge_frame, 35, 3*35, 3)
-
-        cv2.imshow('canny', edges)
-        cv2.waitKey(0)
-
-        line_image = np.copy(edges) * 0
-
-        minLineLength = 20 #img.shape[1] - 300
+        line_image = valid_frame.copy()
         lines = cv2.HoughLinesP(image=edges,
                                 rho=0.65,
                                 theta=np.pi / 113,
                                 threshold=17,
                                 lines=np.array([]),
-                                minLineLength=100,
+                                minLineLength=143,
                                 maxLineGap=4)
-        a, b, c = lines.shape
 
-        for line in lines:
-            for x1,y1,x2,y2 in line:
-                cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),1)
+        if lines is not None:
+            for line in lines:
+                for x1,y1,x2,y2 in line:
+                    dx = x2-x1
+                    dy = y2-y1
 
-        cv2.imshow('line_image', line_image)
-        cv2.waitKey(0)
+                    if y1 < y2:
+                        final1Y = min(y1-dy, int(height*0.4))
+                        final1X = x1+int(dx*(final1Y-y1)/dy)
+                        final2Y = height
+                        final2X = x2+int(dx*(final2Y-y2)/dy)
 
-        # convolution filter
-        # conv_frame = cv2.filter2D(merge_frame, -1, kernel)
+                        cv2.line(line_image,(final1X,final1Y),(final2X,final2Y),(0,255,255),5)
+                    else:
+                        final2Y = max(y2+dy, int(height*0.4))
+                        final2X = x2+int(dx*(final2Y-y2)/dy)
+                        final1Y = height
+                        final1X = x1+int(dx*(final1Y-y1)/dy)
 
-        # cv2.imshow('conv', conv_frame)
-        # cv2.waitKey(0)
+                        cv2.line(line_image,(final1X,final1Y),(final2X,final2Y),(0,255,255),5)
 
-        # initialization for sliding window property
-        # sliding_window = [20, 190, 200, 370]
-        # slide_interval = 15
-        # slide_height = 15
-        # slide_width = 60
-        sliding_window = [300, 600, 370, 520]
+        out.write(line_image)
 
-        print(line_image.shape)
-
-        slide_interval = 15
-        slide_height = 15
-        slide_width = 60
-
-        # initialization for bezier curve variables
-        left_points = []
-        right_points = []
-
-        # define count value
-        count = 0
-        for i in range(line_image.shape[0]-10, 300, -slide_interval):
-            # get edges in sliding window
-            left_edge = line_image[i:i + slide_height, sliding_window[0]:sliding_window[1]].sum(axis=0)
-            right_edge = line_image[i:i + slide_height, sliding_window[2]:sliding_window[3]].sum(axis=0)
-
-            # left railroad line processing
-            if left_edge.argmax() > 0:
-                left_maxindex = sliding_window[0] + left_edge.argmax()
-                left_maxpoint[count] = left_maxindex
-                cv2.line(valid_frame, (left_maxindex, i + int(slide_height / 2)),
-                         (left_maxindex, i + int(slide_height / 2)), (255, 255, 255), 5, cv2.LINE_AA)
-                left_points.append([left_maxindex, i + int(slide_height / 2)])
-                sliding_window[0] = max(0, left_maxindex - int(slide_width / 4 + (slide_width + 10) / (count + 1)))
-                sliding_window[1] = min(390, left_maxindex + int(slide_width / 4 + (slide_width + 10) / (count + 1)))
-                cv2.rectangle(valid_frame, (sliding_window[0], i + slide_height), (sliding_window[1], i), (0, 255, 0),
-                              1)
-
-            # right railroad line processing
-            if right_edge.argmax() > 0:
-                right_maxindex = sliding_window[2] + right_edge.argmax()
-                right_maxpoint[count] = right_maxindex
-                cv2.line(valid_frame, (right_maxindex, i + int(slide_height / 2)),
-                         (right_maxindex, i + int(slide_height / 2)), (255, 255, 255), 5, cv2.LINE_AA)
-                right_points.append([right_maxindex, i + int(slide_height / 2)])
-                sliding_window[2] = max(0, right_maxindex - int(slide_width / 4 + (slide_width + 10) / (count + 1)))
-                sliding_window[3] = min(390, right_maxindex + int(slide_width / 4 + (slide_width + 10) / (count + 1)))
-                cv2.rectangle(valid_frame, (sliding_window[2], i + slide_height), (sliding_window[3], i), (0, 0, 255),
-                              1)
-            count += 1
-
-        # bezier curve process
-        bezier_left_xval, bezier_left_yval = bezier_curve(left_points, 50)
-        bezier_right_xval, bezier_right_yval = bezier_curve(right_points, 50)
-
-        bezier_left_points = []
-        bezier_right_points = []
-        try:
-            old_point = (bezier_left_xval[0], bezier_left_yval[0])
-            for point in zip(bezier_left_xval, bezier_left_yval):
-                cv2.line(valid_frame, old_point, point, (0, 0, 255), 2, cv2.LINE_AA)
-                old_point = point
-                bezier_left_points.append(point)
-
-            old_point = (bezier_right_xval[0], bezier_right_yval[0])
-            for point in zip(bezier_right_xval, bezier_right_yval):
-                cv2.line(valid_frame, old_point, point, (255, 0, 0), 2, cv2.LINE_AA)
-                old_point = point
-                bezier_right_points.append(point)
-        except IndexError:
-            pass
-        '''
-        cv2.imshow('frame', np.vstack([
-            np.hstack([valid_frame,
-                       original_frame,
-                       cv2.cvtColor(histeqaul_frame, cv2.COLOR_GRAY2BGR)]),
-            np.hstack([cv2.cvtColor(blur_frame, cv2.COLOR_GRAY2BGR),
-                       cv2.cvtColor(merge_frame, cv2.COLOR_GRAY2BGR),
-                       cv2.cvtColor(conv_frame, cv2.COLOR_GRAY2BGR)])
-        ]))
-        '''
-        cv2.imshow('Video', valid_frame)
+        cv2.imshow('Video', line_image)
         cv2.waitKey(1)
+
     print('finish')
+
+    out.release()
+    cv2.destroyAllWindows()
 
 
 # class for reading video
@@ -198,45 +99,7 @@ class VideoCapture:
     def read(self):
         # Grab a single frame of video
         ret, frame = self.video.read()
-        return frame is not None, frame
-
-
-# bezier curve function
-def bezier_curve(points, ntimes=1000):
-    """
-       Given a set of control points, return the
-       bezier curve defined by the control points.
-
-       points should be a list of lists, or list of tuples
-       such as [ [1,1],
-                 [2,3],
-                 [4,5], ..[Xn, Yn] ]
-        ntimes is the number of time steps, defaults to 1000
-
-        See http://processingjs.nihongoresources.com/bezierinfo/
-    """
-
-    def bernstein_poly(i, n, t):
-        """
-         The Bernstein polynomial of n, i as a function of t
-        """
-        return comb(n, i) * (t ** (n - i)) * (1 - t) ** i
-
-    nPoints = len(points)
-    xPoints = np.array([p[0] for p in points])
-    yPoints = np.array([p[1] for p in points])
-
-    t = np.linspace(0.0, 1.0, ntimes)
-
-    polynomial_array = np.array([bernstein_poly(i, nPoints - 1, t) for i in range(0, nPoints)])
-
-    xvals = np.dot(xPoints, polynomial_array)
-    yvals = np.dot(yPoints, polynomial_array)
-    return xvals.astype('int32'), yvals.astype('int32')
-
-
-def nothing(value):
-    pass
+        return frame
 
 
 if __name__ == '__main__':
--- algorithm/test.py
+++ algorithm/test.py
@@ -1,13 +1,11 @@
 import cv2
 import numpy as np
 
-def empty(a):
-    pass
+# Image Path selection for testing
+path = 'video/track_test_image_resized.jpg'
+# path = 'video/20220712_075411_resized_cropped.jpg'
 
-
-path = 'video/20220712_075411_resized_cropped.jpg'
-# path = 'video/gray_frame_14750.png'
-# path = 'video/track_test_image_resized.jpg'
+# TrackBars for finding the right parameters
 cv2.namedWindow("TrackBars")
 cv2.resizeWindow("TrackBars",640,240)
 cv2.createTrackbar("rho","TrackBars",65,200,empty)
@@ -16,14 +14,8 @@ cv2.createTrackbar("threshold","TrackBars",20,50,empty)
 cv2.createTrackbar("maxLineGap","TrackBars",10,20,empty)
 cv2.createTrackbar("minLineLength","TrackBars",20,300,empty)
 
-cv2.createTrackbar("threshold_canny","TrackBars",50,1000,empty)
+cv2.createTrackbar("threshold_canny","TrackBars",59,1000,empty)
 cv2.createTrackbar("kernel_size","TrackBars",3,20,empty)
-
-# cv2.createTrackbar("High Threshold","TrackBars",66,1000,empty)
-# cv2.createTrackbar("Sat Min","TrackBars",0,255,empty)
-# cv2.createTrackbar("Sat Max","TrackBars",255,255,empty)
-# cv2.createTrackbar("Val Min","TrackBars",21,255,empty)
-# cv2.createTrackbar("Val Max","TrackBars",255,255,empty)
 
 while True:
     img = cv2.imread(path)
@@ -47,45 +39,29 @@ while True:
 
     h, s, v = imgHSV[:, :, 0], imgHSV[:, :, 1], imgHSV[:, :, 2]
 
-    # cv2.imshow("h", h)
-    # cv2.imshow("s", s)
-    # cv2.imshow("v", v)
     cv2.imwrite("output/h.jpg", h)
     cv2.imwrite("output/s.jpg", s)
     cv2.imwrite("output/v.jpg", v)
 
-    # imgGrayS = cv2.subtract(s, imgGray)
-
-    # h_thres = cv2.getTrackbarPos("High Threshold", "TrackBars")
-    # s_min = cv2.getTrackbarPos("Sat Min", "TrackBars")
-    # s_max = cv2.getTrackbarPos("Sat Max", "TrackBars")
-    # v_min = cv2.getTrackbarPos("Val Min", "TrackBars")
-    # v_max = cv2.getTrackbarPos("Val Max", "TrackBars")
-    
-    # cv2.imshow("gray", imgGray)
-    # cv2.imshow("gray_cropped", imgGray_cropped)
-
     histeqaul_frame = cv2.equalizeHist(imgGray)
 
+    # We subtract the s channel of the HSV colored image to remove
+    # shadows from the image. 
     histeqaul_frame_s = cv2.subtract(histeqaul_frame, s)
-
-    # imgGrayHist = cv2.cvtColor(histeqaul_frame,cv2.COLOR_2GRAY)
-
-    # img_gaussian = cv2.GaussianBlur(histeqaul_frame,(3,3),0)
-
-    # cv2.imshow("hist", histeqaul_frame)
     
     cv2.imwrite("output/hist.jpg", histeqaul_frame_s)
-    # cv2.imshow("gaussian", img_gaussian)
-    # cv2.imwrite("output/gaussian.jpg", img_gaussian)
-
-    #prewitt
-    # kernelx = np.array([[1,1,1],[0,0,0],[-1,-1,-1]])
-    # kernely = np.array([[-1,0,1],[-1,0,1],[-1,0,1]])
-    # # img_prewittx = cv2.filter2D(img_gaussian, -1, kernelx)
-    # img_prewitty = cv2.filter2D(img_gaussian, -1, kernely)
 
     height, width = histeqaul_frame_s.shape
+
+    edges = cv2.Canny(histeqaul_frame_s, threshold_canny, 3*threshold_canny, kernel_size)
+
+    cv2.imshow("canny", edges)
+    cv2.imwrite("output/canny.jpg", edges)
+
+    # We apply a mask to limit the influence of the many small edges of
+    # the trees and other objects aside the tracks.
+
+    # Triangle Shape V1
     # triangle = np.array([
     #                    [
     #                     (int(width/2), int(height/2 - height*0.1)),
@@ -93,96 +69,65 @@ while True:
     #                     (int(width/5)*4, height)
     #                    ]
     #                    ])
-    triangle = np.array([
-                       [
-                        (int(width/2), 0),
-                        (0, height),
-                        (width, height)
-                       ]
-                       ])
+    # Triangle Shape V2
+    # triangle = np.array([
+    #                    [
+    #                     (int(width/2), 0),
+    #                     (0, height),
+    #                     (width, height)
+    #                    ]
+    #                    ])
 
-    mask = np.zeros_like(histeqaul_frame_s)
-    mask = cv2.fillPoly(mask, triangle, 255)
-    
-    # cv2.imshow("mask", mask)
-    cv2.imwrite("output/mask.jpg", mask)
+    # mask = np.zeros_like(histeqaul_frame_s)
+    # mask = cv2.fillPoly(mask, triangle, 255)
 
-    edges = cv2.Canny(histeqaul_frame_s, threshold_canny, 3*threshold_canny, kernel_size)
+    # cv2.imwrite("output/mask.jpg", mask)
+    # masked_edges = cv2.bitwise_and(edges, mask)
 
-    # cv2.imshow("Prewittx", img_prewittx)
-    cv2.imshow("canny", edges)
+    # cv2.imwrite("output/masked_canny.jpg", masked_edges)
 
-    masked_edges = cv2.bitwise_and(edges, mask)
-
-    cv2.imwrite("output/canny.jpg", edges)
-    cv2.imwrite("output/masked_canny.jpg", masked_edges)
-
-    # line_image = np.copy(edges) * 0
-    line_image = masked_edges.copy()
-
-    # minLineLength = 20 #img.shape[1] - 300
-    lines = cv2.HoughLinesP(image=masked_edges,
+    # Draw the detected lines of the rail tracks on top of
+    # the original image
+    line_image = img.copy()
+    lines = cv2.HoughLinesP(image=edges,
                             rho=0.65,
                             theta=np.pi / 113,
                             threshold=17,
                             lines=np.array([]),
-                            minLineLength=100,
+                            minLineLength=143,
                             maxLineGap=4)
 
-    lines = cv2.HoughLinesP(image=masked_edges,
-                            rho=rho/100,
-                            theta=np.pi / theta,
-                            threshold=threshold,
-                            lines=np.array([]),
-                            minLineLength=minLineLength,
-                            maxLineGap=maxLineGap)
-    # a, b, c = lines.shape
+    # Use the line below to control the parameters by the trackbars.
+    # lines = cv2.HoughLinesP(image=masked_edges,
+    #                         rho=rho/100,
+    #                         theta=np.pi / theta,
+    #                         threshold=threshold,
+    #                         lines=np.array([]),
+    #                         minLineLength=minLineLength,
+    #                         maxLineGap=maxLineGap)
 
     for line in lines:
         for x1,y1,x2,y2 in line:
-            cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),5)
+            dx = x2-x1
+            dy = y2-y1
 
-    # for i in range(a):
-    #     cv2.line(line_image, (lines[i][0][0], lines[i][0][1]), (lines[i][0][2],  lines[i][0][3]), (0, 0, 255), 2, cv2.LINE_AA)
+            if y1 < y2:
+                final1Y = min(y1-dy, int(height*0.4))
+                final1X = x1+int(dx*(final1Y-y1)/dy)
+                final2Y = height
+                final2X = x2+int(dx*(final2Y-y2)/dy)
+
+                cv2.line(line_image,(final1X,final1Y),(final2X,final2Y),(0,255,255),5)
+            else:
+                final2Y = max(y2+dy, int(height*0.4))
+                final2X = x2+int(dx*(final2Y-y2)/dy)
+                final1Y = height
+                final1X = x1+int(dx*(final1Y-y1)/dy)
+
+                cv2.line(line_image,(final1X,final1Y),(final2X,final2Y),(0,255,255),5)
 
     cv2.imshow('line_image', line_image)
-    
     cv2.imwrite("output/line_image.jpg", line_image)
-
-    # low_threshold = 50
-    # high_threshold = 150
-    # edges = cv2.Canny(img_gaussian, thres, 3*thres, kernel_size)
-
-    # rho = 5  # distance resolution in pixels of the Hough grid
-    # theta = np.pi / 180  # angular resolution in radians of the Hough grid
-    # threshold = 25  # minimum number of votes (intersections in Hough grid cell)
-    # min_line_length = 300  # minimum number of pixels making up a line
-    # max_line_gap = 20  # maximum gap in pixels between connectable line segments
-    # line_image = np.copy(edges) * 0  # creating a blank to draw lines on
-
-    # # Run Hough on edge detected image
-    # # Output "lines" is an array containing endpoints of detected line segments
-    # lines = cv2.HoughLinesP(edges, rho, theta, threshold, np.array([]),
-    #                     min_line_length, max_line_gap)
-
-    # for line in lines:
-    #     for x1,y1,x2,y2 in line:
-    #         cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),5)
-
-    # lines_edges = cv2.addWeighted(edges, 0.8, line_image, 1, 0)
-
-    # cv2.imshow('edges', edges)
-
-    # print(h_min,h_max,s_min,s_max,v_min,v_max)
-    # lower = np.array([h_min,s_min,v_min])
-    # upper = np.array([h_max,s_max,v_max])
-    # mask = cv2.inRange(imgHSV,lower,upper)
-    # imgResult = cv2.bitwise_and(img,img,mask=mask)
-
-    # cv2.imshow('edge', edges)
-    # cv2.imshow("Mask Images", mask)
-    # cv2.imshow("Original Images", img)
-    
     cv2.imwrite("output/original.jpg", img)
 
     cv2.waitKey(1)
